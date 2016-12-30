4c93120df4e673d9117823a451f5ba1a50ac5b26
Mo Bitar <me@bitar.io>
2016-12-30 09:52:56 -0600

    sync tokens

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -24,12 +24,26 @@ class Api::ItemsController < Api::ApiController
       # force Rails to execute the SQL SELECT command FIRST
       # see http://stackoverflow.com/questions/12052955/rails-activerecord-sql-queries-out-of-order
     end
-    last_updated = DateTime.now + 1/1000
+    last_updated = DateTime.now
     saved_items = _sync_save()
     if saved_items.length > 0
-      last_updated = saved_items.sort_by{|m| m.created_at}.first.updated_at + 1/1000
+      last_updated = saved_items.sort_by{|m| m.created_at}.first.updated_at
     end
-    render :json => {:retrieved_items => retrieved_items, :saved_items => saved_items, :last_updated => last_updated}
+    sync_token = sync_token_from_datetime(last_updated)
+    render :json => {:retrieved_items => retrieved_items, :saved_items => saved_items, :sync_token => sync_token}
+  end
+
+  def sync_token_from_datetime(datetime)
+    version = 1
+    Base64.encode64("#{version}:" + "#{datetime.to_i}")
+  end
+
+  def datetime_from_sync_token(sync_token)
+    decoded = Base64.decode64(sync_token)
+    parts = decoded.rpartition(":")
+    timestamp_string = parts.last
+    date = DateTime.strptime(timestamp_string,'%s')
+    return date
   end
 
   def _sync_save
@@ -60,8 +74,9 @@ class Api::ItemsController < Api::ApiController
   end
 
   def _sync_get
-    if params[:updated_after]
-      items = @user.items.where("updated_at > ?", params[:updated_after].to_time).all
+    if params[:sync_token]
+      date = datetime_from_sync_token(params[:sync_token])
+      items = @user.items.where("updated_at > ?", date).all
     else
       items = @user.items.all
     end
