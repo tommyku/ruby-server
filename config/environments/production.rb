2a29872b356afa42dfe50a260e6dfc0dcfb2b3d9
Jason Kim <iamjsonkim@gmail.com>
2017-01-18 21:01:44 -0800

    Remove unused gems related to Rails view and assets
    update gemlock
    
    remove unrelated things from environment rb
    
    remove bower
    
    added .ruby-version to gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,12 +39,6 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Compress JavaScripts and CSS.
-  config.assets.compress = true
-  # config.assets.js_compressor = Uglifier.new(mangle: false)
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
