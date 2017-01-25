9f48c68d95117e3a0bcdf54709d1236e01b1cbe0
Mo Bitar <me@bitar.io>
2017-01-24 20:55:43 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  # forward non-namespaced routes to api namespace
+  post "items/sync" => "api/items#sync"
+  get 'auth/params' => "api/auth#auth_params"
+  post "auth/sign_in" => "api/auth#sign_in"
+  post "auth" => "api/auth#register"
+
   namespace :api, defaults: {format: :json} do
     get 'auth/params' => "auth#auth_params"
     post "auth/sign_in" => "auth#sign_in"
